78c6449ea150921ecd27011fed8df3a5c2cb466b
cavalcant-e <jonnathy@hotmail.com>
2024-03-16 21:38:01 -0300

    challenge

---
+++ app.js
@@ -0,0 +1,81 @@
+
+
+function limpar( ) {
+  document.getElementById("textEntrada").value = "";
+}
+
+function criptografar() {
+
+   texto = document.getElementById("textEntrada").value;
+   let criptografar
+   //verificar se letras minúsculas e sem acento
+
+   if (/[A-ZÀ-ÖØ-öø-ÿ]/.test(texto)) {
+    alert("Por favor, digite apenas letras minúsculas e sem acento.");
+    return; 
+  }
+  
+    criptografar = texto.replace(/e/g, "enter");
+    criptografar = criptografar.replace(/i/g, "imes");
+    criptografar = criptografar.replace(/a/g, "ai");
+    criptografar = criptografar.replace(/o/g, "ober");
+    criptografar = criptografar.replace(/u/g, "ufat");
+  
+    ocultarImagem()
+
+    document.getElementById("textSaida").value = criptografar
+
+    criptografar = null
+    limpar()
+     
+}
+
+function descriptografar() {
+
+  texto = document.getElementById("textEntrada").value;
+  let descriptografar
+
+    descriptografar = texto.replace(/ai/g, "a");
+    descriptografar = descriptografar.replace(/enter/g, "e");
+    descriptografar = descriptografar.replace(/imes/g, "i");
+    descriptografar = descriptografar.replace(/ober/g, "o");
+    descriptografar = descriptografar.replace(/ufat/g, "u")
+  
+    document.getElementById("textSaida").value = descriptografar
+
+    descriptografar = null
+    limpar()
+    
+}
+
+function copiar() {
+
+  navigator.clipboard.writeText (document.getElementById("textSaida").value)
+mostrarImagem()
+
+}
+
+function ocultarImagem () {
+
+  var imagemSaida = document.getElementById("textSaida");
+  imagemSaida.style.background = "none";
+  
+}
+
+function mostrarImagem () {
+
+  
+  location.reload ()
+ 
+
+}
+
+// function exibirMensagemInicial() {
+//     exibirTextoNaTela('textEntrada', document.querySelector('textEntrada').value;);
+//     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+// }
+
+
+
+
+
